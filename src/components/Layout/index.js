3891e9d8df04f7f4e873ef50ba350416076efb64
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-12-20 03:04:47 +0530

    new: create Layout component

---
+++ src/components/Layout/index.js
@@ -0,0 +1,15 @@
+import Header from '../Header';
+import Playground from '../Playground';
+
+import './styles.scss';
+
+const Layout = () => {
+  return (
+    <div className="layout">
+      <Header />
+      <Playground />
+    </div>
+  );
+};
+
+export default Layout;
