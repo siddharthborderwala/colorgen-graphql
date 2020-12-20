2a45bf3139ad4e7c6171fdfa1d553d51352f91bc
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-12-20 23:55:45 +0530

    update: create ws-link and apolloClient

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
 import Layout from './components/Layout';
 
+const wsLink = new WebSocketLink({
+  uri: process.env.REACT_APP_SERVER_WSL,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      headers: {
+        'x-hasura-access-key': process.env.REACT_APP_ADMIN_SECRET,
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  link: wsLink,
+  cache: new InMemoryCache({
+    typePolicies: {
+      Subscription: {
+        fields: {
+          colors: {
+            merge: (initial, final) => final,
+          },
+        },
+      },
+    },
+  }),
+});
+
 export default function App() {
-  return <Layout />;
+  return (
+    <ApolloProvider client={client}>
+      <Layout />
+    </ApolloProvider>
+  );
 }
